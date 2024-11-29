bed3fd14226a7d8c32e448cd56d5c9856d36de9d
Precious-orji2008 <classicpboy208@gmail.com>
2024-11-29 12:04:01 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Contact form submission handling
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for reaching out! We will contact you soon.');
+  });
+  
